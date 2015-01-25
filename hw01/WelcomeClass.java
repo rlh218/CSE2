f11326971d8960ee82f5e2ae1ad8570877538c43
Rebecca Holowienka <rlh218@lehigh.edu>
2015-01-25 21:32:22 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+///////////////////////
+//Brian Chen
+//Demo 0 
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+ public static void main(String[] args) {
+     
+// print the statement with the box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-R--L--H--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / "); 
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I am from Hamburg, NJ. Hamburg is a small town in northern NJ, and is located within Sussex County. I have lived here my entire life.");
+ }
+}
