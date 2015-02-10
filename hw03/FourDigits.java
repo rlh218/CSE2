558a2bf8de22af9bf00202f3e49518d072784791
Rebecca Holowienka <rlh218@lehigh.edu>
2015-02-10 23:23:12 +0000

    updated FourDigits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -19,17 +19,21 @@ public class FourDigits{
 // add main method
  public static void main(String[] args) {
      
-System.out.printf( %d 0 "Enter a double and display the four digits to the right of the decimal point: "); 
+System.out.printf( "Enter a double and display the four digits to the right of the decimal point: "); 
         //define what the user will be asked to enter
         
     Scanner myScanner = new Scanner(System.in); //define a Scanner
     double num = myScanner.nextDouble(); //collect integer
     
-    int test = (int) (num);
-    double digits = (num % test) * 10000;
-    int digits2 = (int) digits;
-System.out.println("The four digits are: " + digits2);
+    int x = (int) num ;
+    double y = num - x ;
+    int digits = (int) (y*10000);
+    int value1 = (int)(digits/1000)%10;
+    int value2 = (int)(digits/100)%10;
+    int value3 = (int)(digits/10)%10;
+    int value4 = (int)digits%10;
     
+System.out.println("The four digits are: " + value1+value2+value3+value4);
    //test/testtest
    
  } //end of main method
