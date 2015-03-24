7038409ace9eef54eb0136e34439edf77400fa61
Rebecca Holowienka <rlh218@lehigh.edu>
2015-03-24 00:59:49 +0000

    updated BigMac

--- lab03-BigMac/BigMac.java
+++ lab03-BigMac/BigMac.java
@@ -23,22 +23,22 @@ public class BigMac{
     // add main method
     public static void main(String[] args) {
 
-        //declare an instance of the Scanner object
-            Scanner myScanner; //
-
-        //construct an instance of the Scanner class 
-            myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in); //declare scanner
 
             //define variables/ input
                 System.out.print("Enter the number of Big Macs (an integer > 0): ");
+                
                 int nBigMacs = myScanner.nextInt();
+                
                 System.out.print("Enter the cost per Big Mac as" + 
                     " a double (in the form xx.xx): ");
+                    
                 double bigMac$ = myScanner.nextDouble();
-                System.out.print(
-                    "Enter the percent tax as a whole number (xx): ");
+                
+                System.out.print("Enter the percent tax as a whole number (xx): ");
+                
                 double taxRate = myScanner.nextDouble ();
-                taxRate/=100; //user enters percent, but I want proportion
+                taxRate/=100; //user enters percent, but want proportion
                 
             //output
                 double cost$;
@@ -47,19 +47,19 @@ public class BigMac{
                 dimes, pennies; //for storing digits
                     //to the right of the decimal point 
                     //for the cost$ 
-                cost$ = nBigMacs*bigMac$*(1+taxRate);
+                cost$ = nBigMacs * bigMac$ * (1+taxRate);
                     //get the whole amount, dropping decimal fraction
-                dollars = (int)cost$;
+                dollars = (int) cost$;
                     //get dimes amount, e.g., 
                     // (int)(6.73 * 10) % 10 -> 67 % 10 -> 7
                     //  where the % (mod) operator returns the remainder
                     //  after the division:   583%100 -> 83, 27%5 -> 2 
-                dimes = (int)(cost$*10)%10;
-                pennies = (int)(cost$*100)%10;
+                dimes = (int) (cost$ * 10) % 10;
+                pennies = (int) (cost$ * 100) % 10;
                 System.out.println("The total cost of " +nBigMacs
                      + " BigMacs, at $" + bigMac$ +" per bigMac, with a" +  
-                     " sales tax of " + (int)(taxRate*100) + "%, is $" 
-                     +dollars+ '.' +dimes+pennies);
+                     " sales tax of " + (int)(taxRate * 100) + "%, is $" 
+                     + dollars + '.' + dimes + pennies);
                      
     //test/testtest
    
