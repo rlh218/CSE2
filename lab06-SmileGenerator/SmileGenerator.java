f5ee4b0da6daa0042bc17b3655f8baca25b0acb6
Rebecca Holowienka <rlh218@lehigh.edu>
2015-03-24 01:21:06 +0000

    updated SmileGenerator

--- lab06-SmileGenerator/SmileGenerator.java
+++ lab06-SmileGenerator/SmileGenerator.java
@@ -13,36 +13,44 @@
                the number of smiley faces on each line by one for every new line that is used. 
                Use only one loop. */
 
+
 //define a class
 public class SmileGenerator{
+
+//add main method    
+    public static void main(String[] args) {
     
-//add main method
-    public static void main(String[] args){
+    //create loop to print out smileys
+       //declare all variables/inputs
+    	for (int i = 0; i < 5; i++) {
+    		System.out.print(":)");
+		} //close for loop
+		
+    	System.out.println("");//print space
+    	
+    	int random = (int)(Math.random() * 100); //prompting program to have random num of :) for output
+    	    	
+    	for (int j = 0; j < random; j++) { 
+    	    
+    		if (j == 30 || j == 60 || j == 90) {
+    			System.out.println("");//print space
+    		}//close if statement
+    		
+    		System.out.print(":)");
+		}//close for loop
+    	
+    	System.out.println("");//print space
+    	
+    	for (int k = 0; k < random; k++) {
+			for (int l = 0; l < k; l++) {
+				System.out.print(":)");				
+			}//close nested for loop
+			
+			System.out.println("");//print space
+		}//close for loop
+    	
+  
+//testtest
 
-//Step 1: Print out 5 smiley faces using non-nested loops.
-System.out.println(":):):):):)");
-
-//Step 1: Print out 5 smiley faces using while loop.
-int smile = 0;
-while (smile < 5){
-    System.out.print(":)");
-    smile++;
-}
-
-//Step 1: Print out 5 smiley faces using do-while loop.
-//do{
-    
-    
-//}while();
-
-//Step 2: Create a second loop to print out a random number of smiley faces using “math.random()”.
-int smiles = 0; //define variable
-        smiles = (int)((Math.random()*100)+1); 
-        System.out.println(":)");
-        smiles++;
-        
-
-
-
-}
-}
+    }//end of main method
+}//end of class
